b87c36458812a58fc6f4ac643a824ebf4369d49a
dadoo72 <pedrottimail@gmail.com>
2022-08-01 21:31:45 +0200

    connection, try catch, exec

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php 
 
 try{
-	$pdo = new PDO('mysql:host=localhost;dbname=ijdb','ijdbuser','mypassword');
+	$pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
 
-	$output='Database connection established.';
+	// $output='Database connection established.';
+
+	$sql ='UPDATE joke SET jokedate="2021-04-01"
+      WHERE joketext LIKE "%programmer%"';
+
+  $affectedRows = $pdo->exec($sql);
+
+  $output = 'Updated ' . $affectedRows .' rows.';
 }
 catch (PDOException $e){
-	$output = 'Unable to connect to thedatabase server: '.$e->getMessage();
+	// $output = 'Unable to connect to thedatabase server: '.$e->getMessage();
+	$output = 'Database error: ' . $e->getMessage() . ' in ' .
+  $e->getFile() . ':' . $e->getLine();
 }
 
 include __DIR__ . '/../templates/output.html.php';
